bb749cc0b29713471cbf80462609832f81a9a035
Salim <salim.hassan.welleat@gmail.com>
2023-12-15 19:17:07 +0100

    build: electron debugging

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 const { app, BrowserWindow } = require("electron");
+const colors = require("colors");
 
+console.log(colors.rainbow("hello wolrd"));
 //Keep a global reference of the window object, if you don't, the window will be closed automatically when yhe JavaScript object is garbage collected.
 let mainWindow;
 
 //Create a new BrowserWindow when 'app' is ready
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 1000,
+    width: 1600,
     height: 800,
     webPreferences: { nodeIntegration: true },
   });
 
   //Load index.html into the new BrowserWindow
+  mainWindow.loadFile("./index.html");
 
   //Open DevTools - Remove for PROD!
   mainWindow.openDevTools();
 
   //Listen for window being closed
   mainWindow.on("closed", () => {
+    //debugger;
     mainWindow = null;
   });
 }
